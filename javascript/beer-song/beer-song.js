fa6540ad3e84841529d62c4f5a185814d4f1c193
Mia Adjei <adjei.m@gmail.com>
2017-03-21 23:52:36 -0400

    Beer song passes 2 tests.

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -1,7 +1,19 @@
 function BeerSong() {
 
 	this.verse = function(number) {
-		return (number + ' bottles of beer on the wall, ' + number + ' bottles of beer.\nTake one down and pass it around, ' + (number - 1) + ' bottles of beer on the wall.\n');
+
+		var plural = 'bottles';
+		var singular = 'bottle';
+
+		var oneOrMany;
+
+		if (number === 2) {
+			oneOrMany = 'bottle';
+		} else {
+			oneOrMany = 'bottles';
+		}
+
+		return (number + ' bottles of beer on the wall, ' + number + ' bottles of beer.\nTake one down and pass it around, ' + (number - 1) + ' ' + oneOrMany + ' of beer on the wall.\n');
 	};
 
 }
