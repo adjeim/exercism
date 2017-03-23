d7d501c19c43288d0b27e6a29c1ff1aa5d155cb8
Mia Adjei <adjei.m@gmail.com>
2017-03-22 21:21:57 -0400

    Beer song passes 4 tests.

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -2,18 +2,44 @@ function BeerSong() {
 
 	this.verse = function(number) {
 
-		var plural = 'bottles';
-		var singular = 'bottle';
+		// for (var i = 0; i <)
 
-		var oneOrMany;
+		var manyBottles = number + ' bottles of beer on the wall, ' + number + ' bottles of beer.\nTake one down and pass it around, ';
 
-		if (number === 2) {
-			oneOrMany = 'bottle';
-		} else {
-			oneOrMany = 'bottles';
+		// var takeOne;
+
+		var oneLess = (number - 1) + ' bottles of beer on the wall.\n';
+
+		var oneBottleLeft = '1 bottle of beer on the wall.\n';
+
+		var oneBottle = '1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n';
+
+		var noBottles = 'No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n';
+
+		var lyrics = '';
+
+		// var oneOrMany;
+
+		// if (number === 2) {
+		// 	oneOrMany = 'bottle';
+		// } else {
+		// 	oneOrMany = 'bottles';
+		// }
+
+		if (number > 2) {
+			lyrics += manyBottles;
+			lyrics += oneLess;
+		} else if (number === 2) {
+			lyrics += manyBottles;
+			lyrics += oneBottleLeft;
+		} else if (number === 1) {
+			lyrics += oneBottle;
+		} else if (number === 0) {
+			lyrics += noBottles;
 		}
 
-		return (number + ' bottles of beer on the wall, ' + number + ' bottles of beer.\nTake one down and pass it around, ' + (number - 1) + ' ' + oneOrMany + ' of beer on the wall.\n');
+		// return (number + ' bottles of beer on the wall, ' + number + ' bottles of beer.\nTake one down and pass it around, ' + (number - 1) + ' ' + oneOrMany + ' of beer on the wall.\n');
+		return lyrics;
 	};
 
 }
