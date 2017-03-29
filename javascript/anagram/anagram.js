d114f07ec22f4ecd11ffc08192bf5a475a28a844
Mia Adjei <adjei.m@gmail.com>
2017-03-29 01:54:19 -0400

    Complete anagram.js.

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,51 +1,40 @@
 function Anagram(word) {
-	this.matches = function(array) {
+	this.matches = function(compareWords) {
 		var isAnagram = [];
 
+		if (!Array.isArray(compareWords)) {
+			var tempArray = [];
+			for (var a = 0; a < arguments.length; a ++) {
+				tempArray.push(arguments[a]);
+			}
+			compareWords = tempArray;
+		}
 
-		// var originalWord = word;
-
-		for (var i = 0; i < array.length; i ++) {
+		for (var i = 0; i < compareWords.length; i ++) {
 			var originalWord = word.toLowerCase();
 			console.log("the original word is " + originalWord);
 
-			var comparisonWord = array[i].toLowerCase();
+			var comparisonWord = compareWords[i].toLowerCase();
 			console.log("the word to compare is " + comparisonWord);
 
 
 			if (comparisonWord.length === originalWord.length && comparisonWord !== originalWord) {
 
 				for (var k = 0; k < originalWord.length; k ++) {
-					// console.log(comparisonWord.length);
-
+					
 					if (comparisonWord.indexOf(originalWord[k]) > -1 ) {
-						// remove the letter from original word
 						comparisonWord = comparisonWord.replace(originalWord[k], '');
 						console.log(comparisonWord);
-
 					}
-					// console.log(originalWord);
-					// if (originalWord.length === 0) {
-					// 	isAnagram.push(comparisonWord);
-					// }
-
 				}
 				console.log(originalWord);
 				console.log(comparisonWord);
 				if (comparisonWord === '') {
-					isAnagram.push(array[i]);
+					isAnagram.push(compareWords[i]);
 				}
-				// console.log(originalWord)
-				console.log(isAnagram);
-				// return isAnagram;
-
 			}
 		}
-
-		// console.log(array);
 		return isAnagram;
-
-
 	}
 	
 }
