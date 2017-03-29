88cee9d36d6ef223f2b882688e97ba045d0e3811
Mia Adjei <adjei.m@gmail.com>
2017-03-29 01:56:14 -0400

    Clean up extra console.logs.

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -12,23 +12,17 @@ function Anagram(word) {
 
 		for (var i = 0; i < compareWords.length; i ++) {
 			var originalWord = word.toLowerCase();
-			console.log("the original word is " + originalWord);
-
 			var comparisonWord = compareWords[i].toLowerCase();
-			console.log("the word to compare is " + comparisonWord);
 
 
 			if (comparisonWord.length === originalWord.length && comparisonWord !== originalWord) {
-
 				for (var k = 0; k < originalWord.length; k ++) {
 					
 					if (comparisonWord.indexOf(originalWord[k]) > -1 ) {
 						comparisonWord = comparisonWord.replace(originalWord[k], '');
-						console.log(comparisonWord);
 					}
 				}
-				console.log(originalWord);
-				console.log(comparisonWord);
+
 				if (comparisonWord === '') {
 					isAnagram.push(compareWords[i]);
 				}
