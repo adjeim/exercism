b47cf96a63219dfbed94866bf498bd1384737b73
Mia Adjei <adjei.m@gmail.com>
2017-06-02 23:59:01 -0400

    Robot name passes 5 tests.

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -1,19 +1,26 @@
 function Robot() {
 
+	this.usedNames = [];
 	
 	this.generateName = function() {
 		var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 		var numbers = '0123456789';
 		var name = '';
 
-		name += letters.charAt(Math.floor(Math.random() *  26))
-		name += letters.charAt(Math.floor(Math.random() *  26))
-		name += numbers.charAt(Math.floor(Math.random() *  10))
-		name += numbers.charAt(Math.floor(Math.random() *  10))
-		name += numbers.charAt(Math.floor(Math.random() *  10))
+		for (var i = 0; i < 2; i++) {
+			name += letters.charAt(Math.floor(Math.random() *  26))
+		}
 
-		return name;
+		for (var i = 0; i < 3; i++) {
+			name += numbers.charAt(Math.floor(Math.random() *  10))
+		}
 
+		if (this.usedNames.indexOf(name) > -1) {
+			return this.generateName();
+		} else {
+				this.usedNames.push(name);
+				return name;
+		}
 	};
 
 	this.name = this.generateName();
@@ -22,6 +29,8 @@ function Robot() {
 		this.name = this.generateName()
 
 	}
+
+
 
 }
 
