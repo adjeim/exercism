ee6fa9a9cf61649c7918c2113df7730ef45f9d41
Mia Adjei <adjei.m@gmail.com>
2017-06-24 22:32:59 -0400

    Grains passes 7 tests.

--- javascript/grains/grains.js
+++ javascript/grains/grains.js
@@ -1,6 +1,11 @@
 function Grains() {
+	var BigInt = require('./big-integer');
+
 	this.square = (number) => {
-		return (Math.pow(2, number) / 2) .toString();
+		// return (Math.pow(2, number) / 2) .toString();
+		let bigI = BigInt(number);
+		let big2 = BigInt(2);
+		return (big2.pow(bigI).divide(2)).toString();
 
 	};
 
